e0443ed822f015ef28b0b2bca23cb320aaf3d870
Saurav-infostride <saurav.sharma@infostride.com>
2022-01-04 20:48:30 +0530

    test

---
+++ Passes.java
@@ -0,0 +1,17 @@
+public class Passes {
+    public static void main(String [] args)
+    {
+        Passes p = new Passes();
+        p.start();
+    }
+    void start(){
+        String s1 = "slip";
+        String s2 = fix(s1);
+        System.out.println(s1 + " " + s2);
+    }
+    String fix(String s1){
+        s1 = s1 + "stream";
+        System.out.print(s1 + " ");
+        return "stream";
+    }
+}
