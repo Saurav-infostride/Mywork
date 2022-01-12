c90dc5d82719f53d892be47cc1a6dd8b65e7f5f4
SauravSharma <saurav.sharma@infostride.com>
2022-01-12 17:43:34 +0530

    Added first class

---
+++ DiffAssign17.java
@@ -0,0 +1,9 @@
+public class DiffAssign17 {
+    public static void main(String[] str){
+        int a = 5, b = 5;
+        if ( a == b )
+            System.out.print("A and B are equal");
+        else
+            System.out.print("A and B are not equal");
+    }
+}
